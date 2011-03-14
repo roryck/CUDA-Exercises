187f825b0405d4934bdb6ca7865043a9ddc2b5e1
Rory Kelly <rorykelly@gmail.com>
2011-03-14 14:46:47 -0600

    Added some comments to the hello.cu source code.

--- hello/hello.cu
+++ hello/hello.cu
@@ -1,3 +1,10 @@
+/* hello CUDA kernel 
+ * Arguments:
+ *   char *a  - an array of characters
+ * Purpose:
+ *   Each CUDA thread calculates an index value and increments
+ *   its portion of the array by the value of its index.
+ */
 __global__ void hello(char *a)
 {
 	int i = threadIdx.x;
