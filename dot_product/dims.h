1cdd0b21af0d1338c099f6d32b7716471ded23fe
Rory Kelly <rorykelly@gmail.com>
2011-05-26 10:33:35 -0600

    Changed length of vectors in dims.h

--- dot_product/dims.h
+++ dot_product/dims.h
@@ -3,7 +3,7 @@
 
 #include "../common/macros.h"
 
-const int len             = 32341;
+const int len             = 234177;
 const int threadsPerBlock =  32;
 const int blocksPerGrid   = min(64, (len + threadsPerBlock-1)/threadsPerBlock);
 
