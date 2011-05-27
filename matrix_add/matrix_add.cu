c0b9ada7fc7b1f0c723f9b8bffa65586f1eaa6e0
Rory Kelly <rorykelly@gmail.com>
2011-05-27 14:52:39 -0600

    Added files matrix_add.cu and matrix_add.h containing the matrix addtion kernels.  Forgot to add these in the previous commit.

---
+++ matrix_add/matrix_add.cu
@@ -0,0 +1,12 @@
+/*
+ * Kernel routine to add to matrices
+ */
+#include "dims.h"
+__global__ void matrix_add(float *m1, float *m2, float *mout)
+{
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	while(tid < size*size){
+		mout[tid] = m1[tid]+m2[tid];
+		tid += blockDim.x;
+	}
+}
---
+++ matrix_add/matrix_add.h
@@ -0,0 +1,10 @@
+#ifndef _MATRIX_ADD_H_
+#define _MATRIX_ADD_H_
+
+/*
+ * Kernel routine to add to matrices
+ */
+#include "dims.h"
+__global__ void matrix_add(float *m1, float *m2, float *mout);
+
+#endif
