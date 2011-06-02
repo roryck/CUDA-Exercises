c35a788569d7fc77777c6ad5af007119f40938f2
Rory Kelly <rory@login1.longhorn>
2011-06-02 16:19:25 -0500

    Modified run scripts to use $HOME and made them more general

--- dot_product/run-longhorn.sh
+++ dot_product/run-longhorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Example submit command: qsub -V -l h_rt=00:05:00 -P gpgpu -q normal -pe 1way 8 run-longhorn.sh
 
-cd /home/01327/rory/CUDA-Exercises/dot_product
+cd $HOME/CUDA-Exercises/dot_product
 
 ./dot_prod > dot_prod.out
--- matrix_add/run-longhorn.sh
+++ matrix_add/run-longhorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Example submit command: qsub -V -l h_rt=00:05:00 -P gpgpu -q normal -pe 1way 8 run-longhorn.sh
 
-cd /home/01327/rory/CUDA-Exercises/matrix_add
+cd $HOME/CUDA-Exercises/matrix_add
 
 ./matrix_add > matrix_add.out
