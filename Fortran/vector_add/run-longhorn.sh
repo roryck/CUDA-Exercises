b2e4011da350d357accd45666013abfa9a243438
Rory Kelly <rory@login1.longhorn>
2012-02-07 12:50:47 -0600

    Added a CUDA Fortran version of the vector addition example.

---
+++ Fortran/vector_add/run-longhorn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Example submit command: qsub -V -l h_rt=00:05:00 -P gpgpu -q normal -pe 1way 8 run-longhorn.sh
+
+cd $HOME/CUDA-Exercises/Fortran/vector_add
+
+./vec_add > vec_add.out
