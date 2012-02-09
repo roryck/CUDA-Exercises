29132b7cedfaa26ca95ddc18011f5e9716ea57e2
Rory Kelly <rory@login1.longhorn>
2012-02-09 01:28:43 -0600

    Updated run scripts with account key for Feb 2012 tutorial

--- Fortran/vector_add/run-longhorn.sh
+++ Fortran/vector_add/run-longhorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Example submit command: qsub -V -l h_rt=00:05:00 -P gpgpu -q normal -pe 1way 8 run-longhorn.sh
+# Example submit command: qsub -A 20120209NCAR -V -l h_rt=00:05:00 -P gpgpu -q normal -pe 1way 8 run-longhorn.sh
 
 cd $HOME/CUDA-Exercises/Fortran/vector_add
 
